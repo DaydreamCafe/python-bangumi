692962bfd15ce64b879ae6c64346635d6b823257
White_Paper_ <1423634265@qq.com>
2022-02-17 18:06:14 +0800

    构建脚本

--- build.ps1
+++ build.ps1
@@ -1,8 +1,11 @@
+# 删除旧的构建文件
 $TargetFolder = ".\dist" 
 $Files = get-childitem $TargetFolder -force
 Foreach ($File in $Files){
     $FilePath=$File.FullName
     Remove-Item -LiteralPath $FilePath -Recurse -Force
 }
+# 构建
 python setup.py sdist bdist_wheel
+# 上传Pypi
 twine upload dist/*
