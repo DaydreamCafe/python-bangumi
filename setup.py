d9f10a8965574688072bfa14c28f419aaaba35f9
White_Paper_ <1423634265@qq.com>
2022-02-17 17:53:48 +0800

    打包脚本

--- setup.py
+++ setup.py
@@ -6,16 +6,18 @@ with open('./README.md', 'r', encoding='UTF-8') as readme_file:
 
 setuptools.setup(
     name='BangumiAPI',
-    version='0.0.1',
+    version='0.0.2',
     author='WhitePaper233',
     author_email='whitepaper.233@foxmail.com',
     description='番组计划开放API的Python模块',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/WhitePaper233/BangumiAPI",
+    long_description_content_type='text/markdown',
+    url='https://github.com/WhitePaper233/BangumiAPI',
     packages=setuptools.find_packages(),
+    install_requires=['requests>=2.27.1'],
+    python_requires='>=3.10',
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
